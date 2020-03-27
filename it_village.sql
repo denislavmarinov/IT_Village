0db077a72a9ab8336380c456ff0563ef2324f15c
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-03-27 16:09:04 +0200

    Update DB
    Add a new table for game rooms

--- it_village.sql
+++ it_village.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 
--- Версия на сървъра: 5.5.39
--- PHP Version: 5.4.31
+-- Generation Time: Mar 27, 2020 at 03:08 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `it_village`
@@ -23,104 +25,151 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `music`
+-- Table structure for table `game_room`
 --
 
-CREATE TABLE IF NOT EXISTS `music` (
-`music_id` int(11) NOT NULL,
-  `music_name` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE `game_room` (
+  `room_id` int(11) NOT NULL,
+  `player1` int(11) NOT NULL,
+  `player2` int(11) NOT NULL,
+  `player3` int(11) NOT NULL,
+  `player4` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `passwords`
+-- Table structure for table `music`
 --
 
-CREATE TABLE IF NOT EXISTS `passwords` (
-`password_id` int(11) NOT NULL,
+CREATE TABLE `music` (
+  `music_id` int(11) NOT NULL,
+  `music_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `music`
+--
+
+INSERT INTO `music` (`music_id`, `music_name`) VALUES
+(1, 'https://www.youtube.com/watch?v=5FSkEJqO8hA'),
+(2, 'https://www.youtube.com/watch?v=EnflLRtslUQ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passwords`
+--
+
+CREATE TABLE `passwords` (
+  `password_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `password_name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `results`
+-- Table structure for table `results`
 --
 
-CREATE TABLE IF NOT EXISTS `results` (
-`result_id` int(11) NOT NULL,
+CREATE TABLE `results` (
+  `result_id` int(11) NOT NULL,
   `user_name_id` int(11) NOT NULL,
   `wins` int(4) DEFAULT NULL,
   `loss` int(4) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Структура на таблица `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`user_id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
   `user_name` varchar(60) NOT NULL,
   `data_registration` date NOT NULL,
   `data_deleted` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `game_room`
+--
+ALTER TABLE `game_room`
+  ADD PRIMARY KEY (`room_id`),
+  ADD KEY `player1` (`player1`),
+  ADD KEY `player2` (`player2`),
+  ADD KEY `player3` (`player3`),
+  ADD KEY `player4` (`player4`);
+
+--
 -- Indexes for table `music`
 --
 ALTER TABLE `music`
- ADD PRIMARY KEY (`music_id`);
+  ADD PRIMARY KEY (`music_id`);
 
 --
 -- Indexes for table `passwords`
 --
 ALTER TABLE `passwords`
- ADD PRIMARY KEY (`password_id`), ADD UNIQUE KEY `password_name` (`password_name`), ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`password_id`),
+  ADD UNIQUE KEY `password_name` (`password_name`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `results`
 --
 ALTER TABLE `results`
- ADD PRIMARY KEY (`result_id`), ADD KEY `user_name_id` (`user_name_id`);
+  ADD PRIMARY KEY (`result_id`),
+  ADD KEY `user_name_id` (`user_name_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `user_name` (`user_name`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `game_room`
+--
+ALTER TABLE `game_room`
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `music`
 --
 ALTER TABLE `music`
-MODIFY `music_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `music_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `passwords`
 --
 ALTER TABLE `passwords`
-MODIFY `password_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `password_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `results`
 --
 ALTER TABLE `results`
-MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
