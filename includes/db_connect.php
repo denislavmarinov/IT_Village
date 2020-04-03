b666a515fce9b22f78bc28d175035b10e5a5f504
LyubaF <58828846+LyubaF@users.noreply.github.com>
2020-04-03 16:54:18 +0300

    Add files via upload

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,1 +1,9 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'travel');
 
+if( !$conn) {
+	die('Connection failed' . mysqli_connect_error() . ' - ' . mysqli_connect_errno());
+} 
+else {
+	echo "Connected succsessfuly!";
+}
