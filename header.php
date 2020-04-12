fc27f8b6635ba801f24c34f750a81bba2939feaf
LyubaF <58828846+LyubaF@users.noreply.github.com>
2020-04-12 17:37:33 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,70 @@
+<?php 
+session_name('IT_Village');
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
+      <style>
+        body {
+          margin: 0;
+          background: #28a745;
+        }
+      /* Style the header */
+.header {
+  background-color: #f1f1f1;
+  padding: 7px;
+  text-align: center;
+}
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+/* Style the header */
+/*.header {
+  background-color: #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}*/
+
+/* Style the top navigation bar */
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+/* Style the topnav links */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change color on hover */
+/*.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}*/
+</style>
+</head>
+<body>
+ 
+
+
+
+
+
