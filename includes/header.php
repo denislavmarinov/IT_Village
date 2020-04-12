f3335af1c36088379ab9ecbf6244ec7059697e3a
LyubaF <58828846+LyubaF@users.noreply.github.com>
2020-04-12 17:42:52 +0300

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,3 @@
-<?php
-include 'includes/db_connect.php';
-?>
 <?php 
 session_name('IT_Village');
 session_start();
@@ -13,15 +10,14 @@ session_start();
 	<title>Home</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
-    <link rel="stylesheet" type="text/css" href="../style/inner_style.css">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
       <style>
-body {
-  margin: 0;
-  background: #28a745;
-}
-/* Style the header */
+        body {
+          margin: 0;
+          background: #28a745;
+        }
+      /* Style the header */
 .header {
   background-color: #f1f1f1;
   padding: 7px;
@@ -69,16 +65,6 @@ body {
  
 
 
-<!-- <div class="header">
-  <h1>Welcome in IT-Village</h1>
-</div> -->
-<!-- <div class="topnav">
-  <a href="index.php" class="btn btn-info">Home</a>
-    <p><a href="create.php" class="btn btn-warning">Country</a></p>
-    <p><a href="create_destinations.php" class="btn btn-success">Destinations</a></p>
-  <a href="recycle_bin.php" class="btn btn-danger">Recycle bin</a>
-</div>
- -->
 
-</body>
-</html>
+
+
